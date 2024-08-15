519a324b7b6c748d82c94eba1dfb07c4dee2cf51
Thomas <thomas.oberliessen@googlemail.com>
2024-08-15 08:27:06 +0200

    Count lines and bytes

--- wc/main.go
+++ wc/main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	fmt.Printf("Count: %d", count(os.Stdin))
+	lines := flag.Bool("l", false, "Count lines")
+	bytes := flag.Bool("b", false, "Count bytes")
+	flag.Parse()
+	fmt.Printf("Count: %d", count(os.Stdin, *lines, *bytes))
 }
 
-func count(reader io.Reader) int {
+func count(reader io.Reader, countLines bool, countBytes bool) int {
 	scanner := bufio.NewScanner(reader)
-	scanner.Split(bufio.ScanWords)
+	if countLines {
+		scanner.Split(bufio.ScanLines)
+	} else if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	} else {
+		scanner.Split(bufio.ScanWords)
+	}
 	wc := 0
 	for scanner.Scan() {
 		wc++
--- wc/main_test.go
+++ wc/main_test.go
@@ -5,9 +5,25 @@ import (
 	"testing"
 )
 
+func TestCountBytes(t *testing.T) {
+	r := bytes.NewBufferString("Hello")
+	wc := count(r, false, true)
+	if wc != 5 {
+		t.Errorf("Expected 5, got %d", wc)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	r := bytes.NewBufferString("Hello \nWorld. Foo\n Bar")
+	wc := count(r, true, false)
+	if wc != 3 {
+		t.Errorf("Expected 3, got %d", wc)
+	}
+}
+
 func TestCountWords(t *testing.T) {
 	r := bytes.NewBufferString("Hello World")
-	wc := count(r)
+	wc := count(r, false, false)
 	if wc != 2 {
 		t.Errorf("Expected 2, got %d", wc)
 	}
