74fb3bc2e31c0150a578e54d10174882031d5f21
Thomas <thomas.oberliessen@googlemail.com>
2024-09-10 08:09:34 +0200

    Reduce scheduling contention

--- performance/colStats/main.go
+++ performance/colStats/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"runtime"
 	"sync"
 )
 
@@ -47,32 +48,41 @@ func run(filenames []string, op string, column int, out io.Writer) error {
 	resCh := make(chan []float64)
 	errCh := make(chan error)
 	doneCh := make(chan struct{})
+	filesCh := make(chan string)
 
 	wg := sync.WaitGroup{}
 
-	for _, fname := range filenames {
+	go func() {
+		defer close(filesCh)
+		for _, fname := range filenames {
+			filesCh <- fname
+		}
+	}()
+
+	for i := 0; i < runtime.NumCPU(); i++ {
 		wg.Add(1)
 
-		go func(fname string) {
+		go func() {
 			defer wg.Done()
 
-			f, err := os.Open(fname)
-			if err != nil {
-				errCh <- fmt.Errorf("Cannot open file: %w", err)
-				return
-			}
+			for fname := range filesCh {
+				f, err := os.Open(fname)
+				if err != nil {
+					errCh <- fmt.Errorf("Cannot open file: %w", err)
+					return
+				}
 
-			data, err := csv2float(f, column)
-			if err != nil {
-				errCh <- err
-			}
+				data, err := csv2float(f, column)
+				if err != nil {
+					errCh <- err
+				}
 
-			if err := f.Close(); err != nil {
-				errCh <- err
+				if err := f.Close(); err != nil {
+					errCh <- err
+				}
+				resCh <- data
 			}
-
-			resCh <- data
-		}(fname)
+		}()
 	}
 
 	go func() {
