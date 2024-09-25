4ff5940c4d6f7911c5cdb659d1a1b7f05370c4dd
Thomas <thomas.oberliessen@googlemail.com>
2024-09-25 08:54:39 +0200

    Add zsh completions

---
+++ cobra/pScan/cmd/completion.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var completionCmd = &cobra.Command{
+	Use:   "completion",
+	Short: "Generate bash completion for your command",
+	Long: `To load your completions run
+source <(pScan completion)
+
+To load completions automatically on login, add this line to you .bashrc file:
+$ ~/.bashrc
+source <(pScan completion)
+`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return completionAction(os.Stdout)
+	},
+}
+
+func completionAction(out io.Writer) error {
+	return rootCmd.GenZshCompletion(out)
+}
+
+func init() {
+	rootCmd.AddCommand(completionCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// completionCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// completionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
