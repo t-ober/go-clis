ca1d7c78d852c9e127dc1a707a8cb69a96ff53e6
Thomas <thomas.oberliessen@googlemail.com>
2024-09-25 09:01:00 +0200

    Add documentation generation

---
+++ cobra/pScan/cmd/docs.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+)
+
+// docsCmd represents the docs command
+var docsCmd = &cobra.Command{
+	Use:   "docs",
+	Short: "Generate documentation for your command",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		dir, err := cmd.Flags().GetString("dir")
+		if err != nil {
+			return err
+		}
+
+		if dir == "" {
+			if dir, err = os.MkdirTemp("", "pScan"); err != nil {
+				return err
+			}
+		}
+
+		return docsAction(os.Stdout, dir)
+	},
+}
+
+func docsAction(out io.Writer, dir string) error {
+	if err := doc.GenMarkdownTree(rootCmd, dir); err != nil {
+		return err
+	}
+
+	_, err := fmt.Fprintf(out, "Documentation successfully created in %s\n", dir)
+	return err
+}
+
+func init() {
+	rootCmd.AddCommand(docsCmd)
+
+	docsCmd.Flags().StringP("dir", "d", "", "Destination directory for docs")
+}
